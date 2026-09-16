9c3c100b32be09a6f84b53cdc5d46e84d9d79abc
tingkang <liangtingkang8@gmail.com>
2026-09-16 23:53:57 +0800

    challenge 6

---
+++ Challenge 6/Challenge6/cute-dog-cafe/src/App.vue
@@ -0,0 +1,101 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const dogs = ref([
+    { name: 'healthy', age: 3 },
+    { name: 'smiley', age: 7 },
+    { name: 'happy', age: 1 }
+]);
+
+const dogName = ref('');
+const dogAge = ref('');
+
+function addDog() {
+    if (dogName.value != "" && dogAge.value!= "" ){
+        dogs.value.push({name: dogName.value, age:(Number(dogAge.value))})
+        dogName.value =""
+        dogAge.value =""
+    }
+
+    // TODO:
+    // 1. Check that dogName and dogAge are not empty
+    // 2. Add a new dog object to the dogs array
+    // 3. Clear dogName
+    // 4. Clear dogAge
+
+}
+
+</script>
+
+
+<template>
+
+    <h1>🐶 Welcome to Krazy Dog Café!</h1>
+
+    <hr>
+
+    <h2>Our Current Dogs</h2>
+
+    <!-- TODO:
+         Display the dogs as an ordered list.
+
+         Example:
+         Healthy is 3 years old
+         Smiley is 7 years old
+         Happy is 1 year old
+
+         Remember:
+         - Capitalize the first character of the dog's name.
+         - Use "year" when age is 1.
+         - Use "years" for all other ages.
+    -->
+        <ol>
+            <li v-for ="dog of dogs">
+                
+                {{ (dog.name[0].toUpperCase()) }}{{ dog.name.slice(1) }} is {{dog.age}} {{ dog.age === 1 ? "year":"years" }} old
+
+            </li>
+        </ol>
+
+
+    <hr>
+
+    <h2>Add a Dog</h2>
+
+    <!-- TODO:
+         Create inputs for:
+         1. Dog Name
+         2. Dog Age
+    -->
+         Dog Name:<input type="text" v-model="dogName" >
+         <br>
+         Dog Age:<input type ="number" v-model="dogAge">
+
+         <br>
+         <button v-on:click ="addDog">Add Dog</button>
+
+
+    <!-- TODO:
+         Create an Add Dog button.
+         When clicked, call addDog().
+    -->
+
+</template>
+
+
+<style scoped>
+
+h1 {
+    background-color: lightyellow;
+}
+
+input {
+    margin: 5px;
+}
+
+button {
+    margin: 5px;
+}
+
+</style>
